555769b393f34d43e16334317f76fa0557f0f102
Mohit Sharma <msharma3@me.iitr.ac.in>
2024-10-24 03:03:26 +0530

    Time: 5 ms (20.03%), Space: 8.4 MB (42.84%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;  // Get the last digit
+            x /= 10;           // Remove the last digit
+
+            // Check for overflow/underflow before updating `rev`
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+
+            rev = rev * 10 + pop;  // Append the digit to `rev`
+        }
+        return rev;
+    }
+
+    int reverseBruteForce(int x) {
         
         string s = to_string(x);
         string rev;
@@ -11,12 +26,11 @@ public:
         }
 
         while (i<s.size()) {
-            rev = s[i] + rev;
-            i++;
+            rev.insert(0, 1, s[i++]);
         }
 
         if (s[0] == '-') {
-            rev = "-" + rev;
+            rev.insert(0, 1, s[0]);
         }
 
         try {
