b15b6926c41990ae492f012761d2036a295205dd
Mohit Sharma <msharma3@me.iitr.ac.in>
2025-04-01 15:49:14 +0530

    Time: 0 ms (100%), Space: 35.8 MB (52.61%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    static const int M = 1e4;
+    int fwd[M+10];
+    int bwd[M+10];
+    int pivotIndex(vector<int>& nums) {
+        for(int i = 1; i<=nums.size(); i++) {
+            fwd[i] = nums[i-1] + fwd[i-1];
+            bwd[nums.size()-i+1] = nums[nums.size()-i] + bwd[nums.size()-i+2];
+        }
+
+        for(int i = 1; i<=nums.size(); i++) {
+            if (fwd[i-1] == bwd[i+1]) return i-1;
+        }
+
+        return -1;
+    }
+};
